54d3afea0480cb5f3a23084e15d35d018654d78b
lintangg10 <118112684+lintangg10@users.noreply.github.com>
2022-12-02 10:27:21 +0700

    Add files via upload

--- 2_A_71220952.py
+++ 2_A_71220952.py
@@ -1,5 +1,16 @@
 k = input('Masukkan kata: ')
 
 #Cetak_kata
-def cetak_kata(kata):
+def cetak_kata(str): 
+    panjang=len(str) 
+    if panjang % 2 == 0 :
+        return str[0]+str[1]+str[2]+ 'dan' +str[-3]+[-2]+[-1]
+    else:
+        mid= int(panjang/2)
+        return str[mid-1]+str[mid]+str[mid1]
+
+a=input('Masukkan kata: ')
+b=membuathuruf(a)
+print('Huruf yang diambil pada ', a , 'adalah', b)
+
     
